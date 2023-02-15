4e2af4a1167eb885948b830acb5067dd11f1cde1
Kedrorex <belousovvk@bk.ru>
2023-02-15 14:56:02 +0300

    gotovo

---
+++ HJ2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 13: Напишите программу, которая выводит третью
+//  цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+bool status = true;
+while (status)
+{
+    if(number > 99 && number <1000)
+    {
+        result = number % 10;
+        Console.WriteLine($"{result}");
+        status = false;
+    } else if (number < 99)
+    {
+        Console.WriteLine($"третьей цифры нет");
+        status = false;
+    } else
+    {
+       number = number / 10;
+    }
+}
+    
---
+++ HJ3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch (number)
+{
+    case 1:
+        Console.WriteLine("Понедельник - нет");
+        break;
+    case 2:
+        Console.WriteLine("Вторник - нет");
+        break;
+    case 3:
+        Console.WriteLine("Среда - нет");
+        break;
+    case 4:
+        Console.WriteLine("Четверг - нет");
+        break;
+    case 5:
+        Console.WriteLine("Пятница - нет");
+        break;
+    case 6:
+        Console.WriteLine("Суббота - да");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенье - да");
+        break;
+}
